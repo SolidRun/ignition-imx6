0b8b9c214802cdaa62ad4f4f435949458805d777
Rabeeh Khoury <rabeeh@solid-run.com>
2014-10-23 21:51:04 +0300

    Added Arch Linux
    Description from here -
    http://archlinuxarm.org/platforms/armv7/freescale/cubox-i
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

---
+++ ArchLinux/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+dd if=/dev/zero of=/dev/mmcblk0 bs=1M count=4
+echo -e "n\np\n1\n\n\nw\n" | fdisk /dev/mmcblk0
+mkfs.ext4 /dev/mmcblk0p1
+mount /dev/mmcblk0p1 /mnt
+cd /mnt
+curl -L -k http://archlinuxarm.org/os/ArchLinuxARM-imx6-cubox-latest.tar.gz --progress | tar -zxf -
+dd if=boot/SPL of=/dev/mmcblk0 bs=1K seek=1
+dd if=boot/u-boot.img of=/dev/mmcblk0 bs=1K seek=42
+cd /
+umount /mnt
+sync
