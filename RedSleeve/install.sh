852bd2a1628afb8744b17aced7cd1e29abb546ff
Rabeeh Khoury <rabeeh@solid-run.com>
2014-10-26 10:24:23 +0200

    RedSleeve Linux
    Based on images from -
    UK:
    http://www.mirrorservice.org/sites/ftp.redsleeve.org/pub/el6/rootfs/rsel6-cuboxi-20141025.img.xz
    
    NL:
    http://ftp.nluug.nl/os/Linux/distr/redsleeve/el6/rootfs/rsel6-cuboxi-20141025.img.xz
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

---
+++ RedSleeve/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+curl -L -k http://www.mirrorservice.org/sites/ftp.redsleeve.org/pub/el6/rootfs/rsel6-cuboxi-20141025.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
+sync
