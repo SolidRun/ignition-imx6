a3768700451aed94567eb6fe9fb0bbd511e6b65d
Rabeeh Khoury <rabeeh@solid-run.com>
2014-10-27 15:48:29 +0200

    Buildgear.io
    Build Gear is a lightweight build tool for building embedded firmware.
    Images are assembled from -
    http://buildgear.io/prebuilds/hummingboard/mini-distribution/latest/
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

---
+++ BuildGear.io/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+# Wipe out MBR
+dd if=/dev/zero of=/dev/mmcblk0 bs=1k count=1
+echo -e "n\np\n1\n\n\nw\n" | fdisk /dev/mmcblk0
+curl -L -k http://buildgear.io/prebuilds/hummingboard/mini-distribution/latest/SPL | dd of=/dev/mmcblk0 bs=1k seek=1
+curl -L -k http://buildgear.io/prebuilds/hummingboard/mini-distribution/latest/u-boot.img | dd of=/dev/mmcblk0 bs=1k seek=42
+mkfs.ext4 /dev/mmcblk0p1
+mount /dev/mmcblk0p1 /mnt
+cd /mnt
+curl -L -k http://buildgear.io/prebuilds/hummingboard/mini-distribution/latest/fs.tar.xz --progress | unxz | tar -xf -
+cd /
+umount /mnt
+sync
