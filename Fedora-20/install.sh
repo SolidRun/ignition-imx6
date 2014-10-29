f35fa6ca30e4c574b0eb9a8b513284eba356a82b
Rabeeh Khoury <rabeeh@solid-run.com>
2014-10-29 20:39:40 +0200

    Fedora updates
    1. Added default password (fedora).
    2. Add CUSTOM0 to provide the user an option to resize system partition
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- Fedora-20/install.sh
+++ Fedora-20/install.sh
@@ -5,4 +5,9 @@ echo "Downloading and flashing Fedora 20"
 # the bs=1M is critical since it writes much more efficnient to the micro SD
 # Flash the full image
 curl -L -k https://googledrive.com/host/0B0vm64JM4bFZMjFNTGJBT1ozWjg --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
+if [ "x$RESIZE" == "xtrue" ]; then
+	echo -e "d\n3\nn\np\n3\n\n\nw\n" | fdisk /dev/mmcblk0
+	e2fsck -f /dev/mmcblk0p3
+	resize2fs /dev/mmcblk0p3
+fi
 sync
