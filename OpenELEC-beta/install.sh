f1147eabe691bf24f0f1ef8021d5a008853fd48d
Rabeeh Khoury <rabeeh@solid-run.com>
2014-11-06 00:22:23 +0200

    Openelec 5.0 beta 1
    Image from -
    http://releases.openelec.tv/
    
    Official OpenELEC snapshots are removed from RECOMMENDED
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

---
+++ OpenELEC-beta/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+echo "Downloading"
+curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-4.95.1.tar --progress > /tmp/oe.tar
+mkdir -p /tmp/oe
+cd /tmp/oe
+# Untar and remove the leading directory name
+tar --strip-components=1 -xf /tmp/oe.tar
+rm /tmp/oe.tar
+./create_sdcard /dev/mmcblk0
+sync
