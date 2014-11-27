f8ad728eb6a7a261c6dd79b3dd733bce29487112
Rabeeh Khoury <rabeeh@solid-run.com>
2014-11-27 10:15:10 +0200

    Updated OpenSUSE to 13.2
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- OpenSUSE/install.sh
+++ OpenSUSE/install.sh
@@ -2,11 +2,11 @@
 set -e
 # Remove white spaces
 if [ "x$E17" == "xtrue" ]; then
-	IMG=http://files.jm0.eu/suse/openSUSE-13.1-ARM-E17-cuboxi.armv7l-1.12.1-Build68.2.raw.xz
+	IMG=http://files.jm0.eu/suse/openSUSE-13.2-ARM-E17-cuboxi.armv7l.raw.xz
 elif [ "x$XFCE" == "xtrue" ]; then
-	IMG=http://files.jm0.eu/suse/openSUSE-13.1-ARM-XFCE-cuboxi.armv7l-1.12.1-Build68.2.raw.xz
+	IMG=http://files.jm0.eu/suse/openSUSE-13.2-ARM-XFCE-cuboxi.armv7l.raw.xz
 else
-	IMG=http://files.jm0.eu/suse/openSUSE-13.1-ARM-JeOS-cuboxi.armv7l-1.12.1-Build68.2.raw.xz
+	IMG=http://files.jm0.eu/suse/openSUSE-13.2-ARM-JeOS-cuboxi.armv7l.raw.xz
 fi
 curl -L -k $IMG --progress | unxz | dd of=/dev/mmcblk0 bs=1M
 sync
