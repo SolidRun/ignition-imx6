1f5af43918faff807eaaa3a2afed1f8bbead2568
Rabeeh Khoury <rabeeh@solid-run.com>
2014-12-02 18:39:14 +0200

    Fixed Debian-Jessie snapshot
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- Debian-Jessie/install.sh
+++ Debian-Jessie/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Downloading"
-#curl -L -k http://download.solid-run.com/pub/solidrun/cubox-i/Debian/Jessi-repackaged-trial/debian-jessi-4-july-2014.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M
+curl -L -k http://download.solid-run.com/pub/solidrun/cubox-i/Debian/Jessi-repackaged-trial/debian-jessi-4-july-2014.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M
 if [ "x$RESIZE" == "xtrue" ]; then
 	echo -e "d\n2\nn\np\n2\n\n\nw\n" | fdisk /dev/mmcblk0
 	/usr/sbin/e2fsck -f /dev/mmcblk0p2 || true
