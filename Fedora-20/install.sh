426f32a19e992e627421503e7df39d4e1836d7c6
hughbert <craig@bergren.us>
2014-12-20 13:23:09 -0600

    Correct resize algorithm.
    fdisk does not always default the same start sector for a new partition replacing a deleted partition.

--- Fedora-20/install.sh
+++ Fedora-20/install.sh
@@ -6,7 +6,8 @@ echo "Downloading and flashing Fedora 20"
 # Flash the full image
 curl -L -k https://googledrive.com/host/0B0vm64JM4bFZMjFNTGJBT1ozWjg --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
 if [ "x$RESIZE" == "xtrue" ]; then
-	echo -e "d\n3\nn\np\n3\n\n\nw\n" | fdisk /dev/mmcblk0
+        partx --show /dev/mmcblk0 2>&1 | while read NR START END; do : ; done
+        echo -e "d\n${NR}\nn\np\n${NR}\n${START}\n\nw\n" | fdisk /dev/mmcblk0
 	e2fsck -f /dev/mmcblk0p3 || true
 	resize2fs /dev/mmcblk0p3 || true
 fi
