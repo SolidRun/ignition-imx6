de46db51c62593a3ff58e6bcb1ece9b9d79c538b
hughbert <craig@bergren.us>
2014-12-20 21:35:10 -0600

    Fix partition resize algorithm.
    Bug in Fedora documentation on partx, need 1251954, not 1953.  See below.
    
    > partx -g --nr :-1 /dev/mmcblk0
     1    1953 1001953 1000001 488.3M      000ab765-01
     2 1001954 1251953  250000 122.1M      000ab765-02
     3 1251954 3986328 2734375   1.3G      000ab765-03
    
    > partx -g --nr -1:-1 /dev/mmcblk0
     3 1251954 3986328 2734375 1.3G      000ab765-03

--- Fedora-20/install.sh
+++ Fedora-20/install.sh
@@ -10,8 +10,8 @@ echo "Downloading and flashing Fedora 20"
 # Flash the full image
 curl -L -k https://googledrive.com/host/0B0vm64JM4bFZMjFNTGJBT1ozWjg --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
 if [ "x$RESIZE" == "xtrue" ]; then
-	set $(partx -g --nr :-1 /dev/mmcblk0)
-        echo -e "d\n3\nn\np\n3\n${2}\n\nw\n" | fdisk /dev/mmcblk0
+	set $(partx -g --nr -1:-1 /dev/mmcblk0)
+        echo -e "d\n${1}\nn\np\n${1}\n${2}\n\nw\n" | fdisk /dev/mmcblk0
 	e2fsck -f /dev/mmcblk0p3
 	resize2fs /dev/mmcblk0p3
 fi
