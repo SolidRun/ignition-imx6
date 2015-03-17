7bd91b49c9100aeb79e2ba3536a05e90c5d1a4a7
Rabeeh Khoury <rabeeh@solid-run.com>
2015-03-17 22:37:56 +0200

    Updated slackware source file
    Thanks Exaga
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- Slackware/install.sh
+++ Slackware/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading"
-curl -L -k http://humbo.fatdog.eu/files/install/humbo-slackware-install_BETA_02Nov14_fd.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M
+curl -L -k http://humbo.fatdog.eu/files/install/humbo-slackwarearm-installer_latest_fd.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M
 sync
