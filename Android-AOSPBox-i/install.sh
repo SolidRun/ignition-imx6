f89605221974ee65db3d84ddd4cea480f64336f7
Rabeeh Khoury <rabeeh@solid-run.com>
2015-03-31 20:45:52 +0300

    Add data partition resize option
    Resizing the data partition (4th partition) is enabled by default.
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- Android-AOSPBox-i/install.sh
+++ Android-AOSPBox-i/install.sh
@@ -4,4 +4,12 @@ echo "Downloading and flashing Android AOSPBox-i R1.4"
 # Below the --progrss will emit percentage that can be used by the gui to show progress bar
 # the bs=1M is critical since it writes much more efficnient to the micro SD
 curl -L -k http://netcologne.dl.sourceforge.net/project/aospboxi/cuboxi_AOSPBox-i_R1.4.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
+if [ "x$RESIZE" == "xtrue" ]; then
+	# The sector below 100000 is in order to trick fdisk to allocate from
+	# the first available sector after 100000 instead of the beginning
+	# of the volume (which is sector 2048 using fdisk tool).
+        echo -e "d\n4\nn\np\n100000\n\n\nw\n" | fdisk /dev/mmcblk0
+        /usr/sbin/e2fsck -f /dev/mmcblk0p4 || true
+        /usr/sbin/resize2fs /dev/mmcblk0p4 || true
+fi
 sync
