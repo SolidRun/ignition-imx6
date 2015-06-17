2af5f224a0890ad216cbc1aa03102788590735ea
Rabeeh Khoury <rabeeh@solid-run.com>
2015-06-17 18:08:28 +0300

    OpenELEC.tv - switched to use the .img file
    The ./create_sdcard script is no longer part of the tarball
    
    Signed-off-by: Rabeeh Khoury <rabeeh@solid-run.com>

--- OpenELEC.tv.beta/install.sh
+++ OpenELEC.tv.beta/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-echo "Downloading"
-curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-5.95.2.tar --progress > /tmp/oe.tar
-mkdir -p /tmp/oe
-cd /tmp/oe
-# Untar and remove the leading directory name
-tar --strip-components=1 -xf /tmp/oe.tar
-rm /tmp/oe.tar
-./create_sdcard /dev/mmcblk0
+echo "Downloading and installing"
+curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-5.95.2.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
