fea03be056c8e8be81cd1a1e08692fd8db4fe9b3
udK-dev <udK-dev@ch.edu>
2015-07-23 18:11:08 +0200

    Updated some projects
    *Update to Fedora 22
    *Update to latest RedSleeve release
    *Update to Volumio 1.5

--- RedSleeve/install.sh
+++ RedSleeve/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-curl -L -k http://www.mirrorservice.org/sites/ftp.redsleeve.org/pub/el6/rootfs/rsel6-cuboxi-20141025.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k http://www.mirrorservice.org/sites/ftp.redsleeve.org/pub/el6/rootfs/rsel6-cuboxi-latest.img.xz --progress | unxz | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
