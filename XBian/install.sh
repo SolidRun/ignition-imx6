5a54ddaee400a6b4d99697e5acdd77b7d6c89212
Josua Mayer <privacy@not.given>
2015-12-07 16:01:38 +0100

    update xbian download link

--- XBian/install.sh
+++ XBian/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-curl -L -k "http://sourceforge.net/projects/xbian/files/release/XBian_2015.07.11_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k "http://sourceforge.net/projects/xbian/files/release/XBian_2015.12.06_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
