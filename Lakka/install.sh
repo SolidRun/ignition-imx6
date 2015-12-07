4347bfd0b84a3580b03a8856f94cad784c277124
Josua Mayer <privacy@not.given>
2015-12-07 16:47:47 +0100

    Lakka: update download location

--- Lakka/install.sh
+++ Lakka/install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-set -e
-echo "Downloading"
-curl -L -k http://sources.lakka.tv/nightly/${CHOICE} --progress > /tmp/lakka.tar
-mkdir -p /tmp/lakka
-cd /tmp/lakka
-# Untar and remove the leading directory name
-tar --strip-components=1 -xf /tmp/lakka.tar
-./create_sdcard /dev/mmcblk0
-sync
+
+curl -L -k http://sources.lakka.tv/nightly/imx6.arm/${CHOICE} --progress | gzip -d | dd of=/dev/mmcblk0 bs=4M conv=fsync
