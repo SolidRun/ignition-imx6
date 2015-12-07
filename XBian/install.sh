d7b91b4142bb2b3b06ddc621e3b40abb92e2449e
Josua Mayer <privacy@not.given>
2015-12-07 16:04:53 +0100

    xbian: make sure install is reported as failed, when it does

--- XBian/install.sh
+++ XBian/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-set -e
+
+# configure busybox so pipes fail if either element fails
+set -o pipefail
+
+# try to download and flash image, and collect exist status
 curl -L -k "http://sourceforge.net/projects/xbian/files/release/XBian_2015.12.06_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
-sync
+s=$?
+
+# return exit status of download/flashing
+exit $s
