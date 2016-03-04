5673b685d92a5c5ee1a4694152b3c800440b73a8
udK <privacy@none.org>
2016-03-04 18:58:33 +0100

    Update to XBian 2016.03.01

--- XBian/install.sh
+++ XBian/install.sh
@@ -4,7 +4,7 @@
 set -o pipefail
 
 # try to download and flash image, and collect exist status
-curl -L -k "https://sourceforge.net/projects/xbian/files/release/XBian_2016.02.06_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k "https://sourceforge.net/projects/xbian/files/release/XBian_2016.03.01_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 s=$?
 
 # return exit status of download/flashing
