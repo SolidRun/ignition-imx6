a35ca06be7425ebd0a6bf8745a3a13f1d124974b
udK <privacy@none.org>
2016-04-09 15:06:21 +0200

    Update to LibreELEC imx6.arm-7.0-devel-20160408

--- unofficial_LibreELEC.tv/install.sh
+++ unofficial_LibreELEC.tv/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading and installing"
-curl -L -k https://github.com/udK/LibreELEC.tv/releases/download/20160404/LibreELEC-imx6.arm-7.0-devel-20160404160617-r22522-gb0ac12e.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k https://github.com/udK/LibreELEC.tv/releases/download/20160408/LibreELEC-imx6.arm-7.0-devel-20160408203343-r22628-g932cfda.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
