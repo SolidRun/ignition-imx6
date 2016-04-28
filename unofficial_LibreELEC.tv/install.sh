d5c6c504f63e9e3e22fadf938b866d5436732b85
udK <privacy@none.org>
2016-04-28 10:09:54 +0200

    unofficial_LibreELEC.tv is now LibreELEC.tv
    * Official release: LibreELEC (Jarvis) v7.0.0
    * Updated to the new logo
    * News link: https://libreelec.tv/2016/04/libreelec-jarvis-v7-0-0-release/

--- unofficial_LibreELEC.tv/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Downloading and installing"
-curl -L -k https://github.com/udK/LibreELEC.tv/releases/download/20160408/LibreELEC-imx6.arm-7.0-devel-20160408203343-r22628-g932cfda.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
-sync
