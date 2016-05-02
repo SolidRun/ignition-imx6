ab57008e52871a19ca45927d068739c088e67d46
udK <privacy@none.org>
2016-05-02 10:17:11 +0200

    Update to OpenELEC 7.0 beta 3

--- OpenELEC.tv.beta/install.sh
+++ OpenELEC.tv.beta/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading and installing"
-curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-6.95.2.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-6.95.3.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
