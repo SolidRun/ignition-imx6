ec4288e90c8c67bfa4537945ced4c9d4792e79d2
udK <privacy@none.org>
2016-05-18 21:14:13 +0200

    Update to LibreELEC 7.0.1 MR

--- LibreELEC.tv/install.sh
+++ LibreELEC.tv/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading and installing"
-curl -L -k http://releases.libreelec.tv/LibreELEC-imx6.arm-7.0.0.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k http://releases.libreelec.tv/LibreELEC-imx6.arm-7.0.1.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
