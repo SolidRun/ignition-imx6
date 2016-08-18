ba7ea25b31e160d7fe87580aeda497c435562f21
DebtFromAbove <wassregister1@virginmedia.com>
2016-08-18 02:17:18 +0100

    Update install.sh

--- XBian/install.sh
+++ XBian/install.sh
@@ -4,7 +4,7 @@
 set -o pipefail
 
 # try to download and flash image, and collect exist status
-curl -L -k "https://sourceforge.net/projects/xbian/files/release/XBian_Latest_imx6.img/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k "https://sourceforge.net/projects/xbian/files/release/XBian_Latest_imx6.img.gz/download" --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 s=$?
 
 # return exit status of download/flashing
