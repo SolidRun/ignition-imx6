22396d4fe7fde4d787e96a41e71fd7e62238ab4c
halleysfifthinc <halleysfifthinc@users.noreply.github.com>
2017-05-24 14:16:40 -0400

    Update Volumio download to latest version

--- Volumio/install.sh
+++ Volumio/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Remove white spaces
-curl -L -k "http://sourceforge.net/projects/volumio/files/Cubox-i/1.5/Volumio1.5Cuboxi.img.zip/download" --progress > /tmp/volumio.zip
+curl -L -k "http://updates.volumio.org/cuboxi/volumio/2.129/volumio-2.129-2017-03-26-cuboxi.img.zip" --progress > /tmp/volumio.zip
 cd /tmp
-unzip volumio.zip Volumio1.5Cuboxi.img -p | dd of=/dev/mmcblk0 bs=1M conv=fsync
+unzip volumio.zip volumio-2.129-2017-03-26-cubox.img -p | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
