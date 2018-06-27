66e8db8fc741b8d6126f544ac0353ab4c24beb48
Alessio Adamo <alessio.adamo@gmail.com>
2018-06-27 17:53:21 +0200

    Create install.sh

---
+++ openSUSE-Leap-15.0/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+# Remove white spaces
+if [ "x$E20" == "xtrue" ]; then
+	IMG=http://download.opensuse.org/ports/armv7hl/distribution/leap/15.0/appliances/openSUSE-Leap15.0-ARM-E20-cuboxi.armv7l-2018.05.20-Buildlp150.6.2.raw.xz
+elif [ "x$XFCE" == "xtrue" ]; then
+	IMG=http://download.opensuse.org/ports/armv7hl/distribution/leap/15.0/appliances/openSUSE-Leap15.0-ARM-XFCE-cuboxi.armv7l-2018.05.20-Buildlp150.6.1.raw.xz
+elif [ "x$LXQt" == "xtrue" ]; then
+	IMG=http://download.opensuse.org/ports/armv7hl/distribution/leap/15.0/appliances/openSUSE-Leap15.0-ARM-LXQT-cuboxi.armv7l-2018.05.20-Buildlp150.6.1.raw.xz
+elif [ "x$KDE" == "xtrue" ]; then
+	IMG=http://download.opensuse.org/ports/armv7hl/distribution/leap/15.0/appliances/openSUSE-Leap15.0-ARM-KDE-cuboxi.armv7l-2018.05.20-Buildlp150.6.2.raw.xz
+else
+	IMG=http://download.opensuse.org/ports/armv7hl/distribution/leap/15.0/appliances/openSUSE-Leap15.0-ARM-JeOS-cuboxi.armv7l-2018.05.20-Buildlp150.6.2.raw.xz
+fi
+curl -L -k $IMG --progress | unxz | dd of=/dev/mmcblk0 bs=4M iflag=fullblock oflag=direct
+sync
