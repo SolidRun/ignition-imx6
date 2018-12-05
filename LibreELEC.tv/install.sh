02b03b59d262bb02285a09b84ded560abfa5a3c5
Alessio Adamo <alessio.adamo@gmail.com>
2018-12-05 09:48:45 +0100

    Automatically get the image name
    Get the current image name by parsing the html code of the download page.
    It was hardcoded version 7.0.1 (broken link), now at version 8.2.5.

--- LibreELEC.tv/install.sh
+++ LibreELEC.tv/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+
+IMG=$(wget -q https://libreelec.tv/downloads/cubox-i2-i4-hummingboard/ -O - | grep -oh '<a href=.*.img.gz">' | cut -f2 -d'"')
+
 echo "Downloading and installing"
-curl -L -k http://releases.libreelec.tv/LibreELEC-imx6.arm-7.0.1.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k ${IMG} --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
